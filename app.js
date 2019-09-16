7ec5b839c65ebf721093f857348dccdec90b3b04
Bukacz <mbukacz@gmail.com>
2019-09-16 22:17:33 +0200

    processing payments with stripe

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const bookingRouter = require('./routes/bookingRoutes');
 const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
@@ -81,6 +82,7 @@ app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
+app.use('/api/v1/bookings', bookingRouter);
 
 // middleware handling unhandled routes - ALWAYS SHOULD STAND AT THE LAST PLACE OF THE ROUTES!
 app.all('*', (req, res, next) => {
---
+++ public/js/stripe.js
@@ -0,0 +1,22 @@
+/* eslint-disable */
+import axios from 'axios';
+import { showAlert } from './alerts';
+
+const stripe = Stripe('pk_test_Zi6NQYuSHLD9enrCpdv9EPSS00quvOIXM0');
+
+export const bookTour = async tourId => {
+  try {
+    // 1) Get checkout session from API endpoint
+    const session = await axios(
+      `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`
+    );
+
+    // 2) Create checkout form + process + charge credit card
+    await stripe.redirectToCheckout({
+      sessionId: session.data.session.id
+    });
+  } catch (err) {
+    console.log(err);
+    showAlert('error', err);
+  }
+};
