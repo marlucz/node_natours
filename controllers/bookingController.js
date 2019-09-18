733e0d969b953f8bd05b601a46a010042750ba05
Bukacz <mbukacz@gmail.com>
2019-09-18 20:43:19 +0200

    fix #1 stripe implementation

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -6,24 +6,21 @@ const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
 
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
-  // 1) Get currently booked tour
-
+  // 1) Get the currently booked tour
   const tour = await Tour.findById(req.params.tourId);
+  // console.log(tour);
 
   // 2) Create checkout session
-
   const session = await stripe.checkout.sessions.create({
-    // information about checkout session
     payment_method_types: ['card'],
-    // success_url: `${req.protocol}://${req.get('host')}/?tour=${
+    // success_url: `${req.protocol}://${req.get('host')}/my-tours/?tour=${
     //   req.params.tourId
     // }&user=${req.user.id}&price=${tour.price}`,
-    success_url: `${req.protocol}://${req.get('host')}/my-tours`,
+    success_url: `${req.protocol}://${req.get('host')}/my-tours?alert=booking`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
-    // informations about the product itself
-    display_items: [
+    line_items: [
       {
         name: `${tour.name} Tour`,
         description: tour.summary,
@@ -36,23 +33,13 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
       }
     ]
   });
-  // 3) Create session as response
 
+  // 3) Create session as response
   res.status(200).json({
     status: 'success',
     session
   });
 });
-
-// exports.createBookingCheckout = catchAsync(async (req, res, next) => {
-//   // This is only TEMPORARY SOLUTION, it's INSECURE
-//   const { tour, user, price } = req.query;
-//
-//   if (!tour && !user && !price) return next();
-//   await Booking.create({ tour, user, price });
-//
-//   res.redirect(req.originalUrl.split('?')[0]);
-// });
 
 const createBookingCheckout = async session => {
   const tour = session.client_reference_id;
@@ -65,7 +52,6 @@ exports.webhookCheckout = (req, res, next) => {
   const signature = req.headers['stripe-signature'];
 
   let event;
-
   try {
     event = stripe.webhooks.constructEvent(
       req.body,
